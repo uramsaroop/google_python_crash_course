735b6e4f2493ee5a57f701c4c10d6f7768c937e3
uramsaroop <umesh.ramsaroop@gmail.com>
2020-05-20 23:06:16 -0400

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,8 @@
+def first_and_last(message):
+  if message[0] == message[-1]:
+    return True
+  elif message[0] != message[-1]:
+    return False
+
+print(first_and_last("else"))
+print(first_and_last("tree"))
